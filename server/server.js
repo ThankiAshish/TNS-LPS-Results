57bf81614ada3121fbb6c82bb3402b0212addfe7
= <thankiashish555@gmail.com>
2023-01-10 11:10:56 +0530

    Conversion Working!

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import fileUpload from "express-fileupload";
 import cors from "cors";
 import path from "path";
+import puppeteer from "puppeteer";
+import csv from "csvtojson";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -9,7 +11,6 @@ const __dirname = path.dirname(__filename);
 
 import filePayloadExists from "./Middleware/filePayloadExists.js";
 import fileSizeLimiter from "./Middleware/fileSizeLimiter.js";
-import converter from "./Converter/converter.js";
 
 const PORT = 5000;
 
@@ -36,9 +37,51 @@ app.post(
   }
 );
 
-app.get("/convert", (req, res) => {
-  converter();
-  console.log("Done");
+app.get("/convert", async (req, res) => {
+  function chunk(items, size) {
+    const chunks = [];
+    items = [].concat(...items);
+
+    while (items.length) {
+      chunks.push(items.splice(0, size));
+    }
+
+    return chunks;
+  }
+
+  async function ConvertToPDF(url, fileName) {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    await page.goto(url, { waitUntil: "networkidle2" });
+
+    await page.pdf({
+      path: `./PDFs/${fileName}`,
+      format: "A4",
+      width: 1920,
+      height: 1080,
+      printBackground: true,
+      margin: {
+        top: "0.53in",
+        bottom: "0.53in",
+        left: "0.55in",
+        right: "0.55in",
+      },
+    });
+
+    await browser.close();
+  }
+
+  const filePath = "./Data/Data.csv";
+  const jsonData = await csv().fromFile(filePath);
+  const chunks = chunk(jsonData, 5);
+
+  chunks.forEach((chunk) => {
+    chunk.map((data) => {
+      const fileName = `${data.global_id}_${data.first_name}_${data.last_name}.pdf`;
+      ConvertToPDF(data.url, fileName);
+    });
+  });
 });
 
 app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`));
