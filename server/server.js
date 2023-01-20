863c0db673982d54f525c4d3a2aa8fec37663dad
namanthanki <stuniz7855@gmail.com>
2023-01-20 14:02:04 +0530

    fixed unhandled promise for convert endpoint

--- server/server.js
+++ server/server.js
@@ -121,27 +121,28 @@ app.post("/convert", async (req, res) => {
           }) 
           .catch(e => {
             console.log(e);
-            mailsNotSent.push(data.email + '->' + fileName);
-            res.json({
-              status: "error",
-              message: "Something Went Wrong!",
-              filesOk: filesConverted,
-              filesNotOk: filesNotConverted,
-              mailsOk: mailsSent,
-              mailsNotOk: mailsNotSent,
-              totalRows: jsonData.length,
-              scannedRows: file_count,
-            })
+            mailsNotSent.push(data.email + '->' + fileName)
           });
         }
         filesConverted.push(data.global_id);
         file_count++;
       } catch (err) {
-        console.log(err);
+        console.log("here", err);
         filesNotConverted.push(data.global_id);
         if(flag) {
           mailsNotSent.push(data.email + '->' + fileName);
         }
+
+        res.json({
+          status: "error",
+          message: "Something Went Wrong!",
+          filesOk: filesConverted,
+          filesNotOk: filesNotConverted,
+          mailsOk: mailsSent,
+          mailsNotOk: mailsNotSent,
+          totalRows: jsonData.length,
+          scannedRows: file_count,
+        })
       }
     }
   }
